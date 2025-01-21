0f68da600cb0a8f17a329a2c9bcc9073a4e3bb27
thebearguy <bugbreaker18@gmail.com>
2025-01-21 05:56:23 +0530

    debug job fetching wrapper

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -1,9 +1,18 @@
 import supabaseClient from "@/utils/supabase";
 
-export async function getJobs(token) {
+export async function getJobs(token, {location, company_id, searchQuery}) {
     const supabase = await supabaseClient(token)
-
-    const {data, error} = await supabase.from("jobs").select("*");
+    let query = supabase.from("jobs").select("*");
+    if (location) {
+        query = query.eq("location", location);
+    }
+    if (company_id) {
+        query = query.eq("company_id", company_id);
+    }
+    if (searchQuery) {
+        query = query.ilike("title", `%${searchQuery}%`);
+    }
+    const {data, error} = query;
     if (error) {
         console.error("ERROR FETCHING THE JOBS FROM DB :: ", error);
         return null;
--- src/pages/JobListing.jsx
+++ src/pages/JobListing.jsx
@@ -1,28 +1,55 @@
 import { getJobs } from '@/api/apiJobs'
-import { useSession } from '@clerk/clerk-react'
-import React, { useEffect } from 'react'
+import useFetch from '@/hooks/useFetch';
+import { useSession, useUser } from '@clerk/clerk-react'
+import React, { useEffect, useState } from 'react'
+import { BarLoader } from 'react-spinners';
 
 function JobListing() {
-    const {session}  = useSession();
-    console.log(session);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [location, setLocation] = useState("");
+    const [company_id, setCompany_id] = useState("");
+    const {isLoaded}  = useUser();
+    const {fn: fnJobs, data: jobs, loading: loadingJobs} = useFetch(getJobs,{location,  company_id, searchQuery} )
+    console.log(jobs);
 
-    async function fetchJobs() {
-        const supabaseAccessToken = await session.getToken({
-            template: "supabase",
-        });
-        console.log("HELLO JIIII");
+    useEffect(()=> {
+        if(isLoaded) {
+            fnJobs();
+        }
+    }, [isLoaded, location, searchQuery, company_id]);
 
-        const data = await getJobs(supabaseAccessToken)
-        console.log(data);
+    if (!isLoaded) {
+        return <BarLoader className='mb-4' width={"100%"} color='#36d7b7'   />;
     }
-    useEffect(() => {
-        fetchJobs();
-    }, [session])
-
 
   return (
     <div>
+        <h1 className='gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8'>Latent Jobs</h1>
 
+        {/* Add filters here */}
+
+
+        {loadingJobs && (
+            <BarLoader className='mt-4' width={"100%"} color='#36d7b7' />
+        )}
+
+       {loadingJobs === false && (
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {jobs?.length ? (
+                jobs.map((job) => {
+                return (
+                    <JobCard
+                    key={job.id}
+                    job={job}
+                    savedInit={job?.saved?.length > 0}
+                    />
+                );
+                })
+                ) : (
+                <div>No Jobs Found 😢</div>
+            )}
+        </div>
+      )}
     </div>
   )
 }
